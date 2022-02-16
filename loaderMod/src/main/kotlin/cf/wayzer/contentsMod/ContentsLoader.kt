b6b9d6f6ca9aee7ff5a81474f256328289295d0c
way-zer <himc.wicp@gmail.com>
2022-02-16 21:02:31 +0800

    修复指令和资源包加载时机问题

--- loaderMod/src/main/kotlin/cf/wayzer/contentsMod/ContentsLoader.kt
+++ loaderMod/src/main/kotlin/cf/wayzer/contentsMod/ContentsLoader.kt
@@ -1,24 +1,69 @@
 package cf.wayzer.contentsMod
 
-import arc.Events
+import arc.func.Cons
+import arc.func.Prov
+import arc.struct.ObjectMap
+import arc.struct.Seq
 import arc.util.Log
 import mindustry.Vars
 import mindustry.ctype.Content
 import mindustry.ctype.ContentList
 import mindustry.ctype.ContentType
-import mindustry.game.EventType.ResetEvent
 import mindustry.gen.Call
+import mindustry.gen.ClientPacketReliableCallPacket
 import mindustry.mod.Mod
+import mindustry.net.Net
+import mindustry.net.Packet
+import mindustry.net.Packets
 import kotlin.system.measureTimeMillis
+
+class MYClientPacketReliableCallPacket : ClientPacketReliableCallPacket() {
+    override fun getPriority(): Int {
+        if (type == "ContentsLoader|load") return Packet.priorityHigh
+        return super.getPriority()
+    }
+}
 
 @Suppress("unused")
 class ContentsLoader : Mod() {
     override fun init() {
-        Events.on(ResetEvent::class.java) {
+        Vars.content = MyContentLoader
+
+        val clientListeners = Net::class.java.getDeclaredField("clientListeners").run {
+            isAccessible = true
+            @Suppress("UNCHECKED_CAST")
+            get(Vars.net) as ObjectMap<Class<*>, Cons<Packet>>
+        }
+        val bak = clientListeners[Packets.WorldStream::class.java]!!
+        Vars.net.handleClient(Packets.WorldStream::class.java) {
+            beforeWorldLoad()
+            bak.get(it)
+        }
+
+        val packetProvs = Net::class.java.getDeclaredField("packetProvs").run {
+            isAccessible = true
+            @Suppress("UNCHECKED_CAST")
+            get(null) as Seq<Prov<out Packet>>
+        }
+        packetProvs[Net.getPacketId(ClientPacketReliableCallPacket()).toInt()] = ::MYClientPacketReliableCallPacket
+        Vars.netClient.addPacketHandler("ContentsLoader|load") {
+            Log.infoTag("ContentsLoader", "ToLoad $it")
+            Call.serverPacketReliable("ContentsLoader|load", loadType(it))
+        }
+        Log.infoTag("ContentsLoader", "Finish Load Mod")
+    }
+
+    @Suppress("unused", "MemberVisibilityCanBePrivate")
+    companion object API {
+        infix fun ContentList.eq(contentList: ContentList?): Boolean {
+            return javaClass == contentList?.javaClass
+        }
+
+        fun beforeWorldLoad() {
             //fastPath
             if (MyContentLoader.contents.all { it.content eq it.lastContent }) {
                 MyContentLoader.contents.forEach { it.content = it.default }
-                return@on
+                return
             }
             MyContentLoader.contents.forEach {
                 val time = measureTimeMillis { it.load() }
@@ -34,19 +79,6 @@ class ContentsLoader : Mod() {
                 }
                 Log.infoTag("ContentsLoader", "Content.load costs ${timeLoad}ms")
             }
-        }
-        Vars.content = MyContentLoader
-        Vars.netClient.addPacketHandler("ContentsLoader|load") {
-            Log.infoTag("ContentsLoader", "ToLoad $it")
-            Call.serverPacketReliable("ContentsLoader|load", loadType(it))
-        }
-        Log.infoTag("ContentsLoader", "Finish Load Mod")
-    }
-
-    @Suppress("unused", "MemberVisibilityCanBePrivate")
-    companion object API {
-        infix fun ContentList.eq(contentList: ContentList?): Boolean {
-            return javaClass == contentList?.javaClass
         }
 
         fun maskChanged(type: ContentType) {
